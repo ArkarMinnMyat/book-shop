5ff8aa83253c5b7964306354d221ad0cafb1187e
HackerMgPyone <arkarminmyat121@gmail.com>
2024-01-11 19:19:28 +0630

    view cart

---
+++ src/main/java/com/example/bookshop/service/CartService.java
@@ -0,0 +1,39 @@
+package com.example.bookshop.service;
+import com.example.bookshop.dto.CartItem;
+import com.example.bookshop.entity.Book;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+@RequiredArgsConstructor
+public class CartService {
+    private final CartBean cartBean;
+
+    public void addToCart(Book book){
+        cartBean.addCartItem(toCardItem(book));
+    }
+
+    public Set<CartItem> getCartItems(){
+        return cartBean.getCartItems();
+    }
+
+    public Integer cartSize(){
+        return cartBean.cartSize();
+    }
+
+    private CartItem toCardItem(Book book){
+        return new CartItem(
+                book.getId(),
+                book.getIsbn(),
+                book.getTitle(),
+                book.getPrice(),
+                1
+        );
+    }
+
+    public void deleteCartItem(int id, String isbn){
+        cartBean.deleteCartItem(id,isbn);
+    }
+}
